7af3e5c4ab0ac453f8c6f3ee95c8e65f6b76df7a
faredamo <faridamohamed229419@gmail.com>
2026-07-26 05:30:20 +0300

    Solve Task 3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 class Program
 {
@@ -22,5 +23,14 @@ class Program
         Console.WriteLine(ReferenceEquals(firstLine, secondLine));
 
         #endregion
+
+        #region 3
+        //Create a StringBuilder, Append() the text "Book List", then Append() " - Updated" onto the same object. Print the final result
+        StringBuilder s1 = new StringBuilder();
+        s1.Append("Book List");
+        s1.Append("Updated");
+        Console.WriteLine(s1);
+        #endregion
     }
 }
+

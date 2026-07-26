75e6ea70cc35e669040148b7c3defbac0ed671e8
faredamo <faridamohamed229419@gmail.com>
2026-07-26 03:47:15 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        #region 1
+
+        //Declare string title = "clean code";. Call title.ToUpper() and store it in a new variable upperTitle. Print both title and upperTitle to show that title did not change
+        string title = "clean code";
+        string upperTitle = title.ToUpper();
+        Console.WriteLine(title);
+        Console.WriteLine(upperTitle);
+
+        #endregion
+    }
+}
